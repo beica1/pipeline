231799a47b31899cbd9f77ca4beffc16f6c130be
beicai <zhouyii26@hotmail.com>
2019-10-25 17:54:28 +0800

    init

---
+++ src/Panel.js
@@ -0,0 +1,10 @@
+/**
+ * Panel.js of pipleline
+ * Created by beica on 2019/10/25
+ */
+import React from 'react'
+import './Panel.scss'
+
+export default () => {
+  return <div className="panel frosted">控制面板</div>
+}
